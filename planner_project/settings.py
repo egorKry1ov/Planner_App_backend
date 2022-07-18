377ac91025629782c9198ac316357c98b6678ea3
egorkrylov <krylov.yegor96@gmail.com>
2022-07-17 23:50:24 -0400

    updated settings

--- planner_project/settings.py
+++ planner_project/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = "app-root/repo/wsgi/static"
 
 STATIC_URL = 'static/'
 
